23dbb5a5e98ca9072ac1befc809fc2c10ab3fa0f
Steven Scherber <sscherber@comcast.net>
2021-10-17 20:41:27 -0700

    Initial MeetingWindow Setup
    Still pretty early days here, but at this point we've got a simple Character prefab, which has a sprite renderer and animator component. We'll ba manually creating thew animations using the frames Oz provides, which will give us a lot of freedom about the timing. Since we're using Sprites, we'll also be able to mess with stuff later on. Anyway, currently have a "Video Feed" parent object, which will house the animated components. We'll need some additional fluff, as well as a simple button set up for starting a meeting when allowed.

---
+++ Assets/Scripts/MeetingSystem/Character.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public string characterName;
+
+    public SpriteRenderer spriteRenderer;
+
+    public Animator animator;
+}
