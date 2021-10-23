01c222474223ced84e9447554de409d143950888
Steven Scherber <sscherber@comcast.net>
2021-10-22 17:50:25 -0700

    Revert "Added UI Icons but also the Text Architect is broken now"
    This reverts commit 02c8d42d47ed65d6392be71ba3c582a3547f399f.

--- Assets/Scripts/DialogueSystem/TextArchitect.cs
+++ Assets/Scripts/DialogueSystem/TextArchitect.cs
@@ -49,8 +49,8 @@ public class TextArchitect
 		tmpro.text += preText;
 
 		tmpro.ForceMeshUpdate(false);
-		var inf = tmpro.textInfo;
-        int vis = inf.characterCount;
+		TMP_TextInfo inf = tmpro.textInfo;
+		int vis = inf.characterCount;
 
 		tmpro.text += targetText;
 
